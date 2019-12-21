40b728c99248aed568dde94fdfe4b050c143be25
Brittany Dockter <bwitdoc@gmail.com>
2019-12-20 17:20:07 -0800

    adding assignment 1.5

---
+++ week-1/dockter-hello-modules/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1> Welcome from the AppComponent </h1>
+    <app-shipping></app-shipping>`,
+ styles: []
+})
+export class AppComponent {
+  title = 'dockter-hello-modules';
+}
