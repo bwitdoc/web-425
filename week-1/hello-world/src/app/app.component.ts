21312592b8e2f2a6dccc56dd23ece2e67fc37d9b
Brittany Dockter <bwitdoc@gmail.com>
2019-12-20 16:36:26 -0800

    adding assignment 1.3

---
+++ week-1/hello-world/src/app/app.component.ts
@@ -0,0 +1,20 @@
+/*
+===================
+Title: app.component.ts
+Author: Professor Krasso
+Modified by: Brittany Dockter
+Date: December 20 2019
+Description: Assignment 1.3
+===================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello World!';
+}
