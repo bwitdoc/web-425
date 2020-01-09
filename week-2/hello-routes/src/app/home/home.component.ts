f193edf82cf791ff1909b854c65d77bb89a1af36
Brittany Dockter <bwitdoc@gmail.com>
2020-01-09 14:47:11 -0800

    adding week 2 assignments

---
+++ week-2/hello-routes/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <div class="container">
+      <div class="row">
+        <h2> Hello and Welcome to the Home page! </h2>
+        </div>
+      </div>
+  `,
+  styles: [`
+  .container {
+    margin-top: 20px;
+  }
+  h2 {
+    color: darkorange;
+  }
+  `]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
