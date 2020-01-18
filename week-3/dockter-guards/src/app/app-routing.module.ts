c464b0f3ebbe0fce10d88d03724906b11c25c6b6
Brittany Dockter <bwitdoc@gmail.com>
2020-01-18 10:23:19 -0800

    adding week 3 assignments

---
+++ week-3/dockter-guards/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './login.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { UnsavedChangesGuard } from './unsaved-changes.guard';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductDetailComponent, canActivate: [LoginGuard], canDeactivate: [UnsavedChangesGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ week-3/dockter-guards/src/app/app.module.ts
@@ -0,0 +1,32 @@
+  
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {UnsavedChangesGuard} from "./unsaved-changes.guard";
+import {LoginGuard} from "./login.guard";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy}, LoginGuard, UnsavedChangesGuard], //must add canactivate and candeactivate for injections to work
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
