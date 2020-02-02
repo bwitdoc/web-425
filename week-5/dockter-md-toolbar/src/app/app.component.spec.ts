aab9d618c224fb790c4f68b7256f0cdc11db89e8
Brittany Dockter <bwitdoc@gmail.com>
2020-02-02 08:30:25 -0800

    updating week 5 assignments

--- week-5/dockter-md-toolbar/src/app/app.component.spec.ts
+++ week-5/dockter-md-toolbar/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('dockter-md-toolbar');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dockter-md-toolbar app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dockter-md-toolbar!');
   });
 });
--- week-5/dockter-md-toolbar/src/app/app.module.ts
+++ week-5/dockter-md-toolbar/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
-import { MatMenuModule } from '@angular/material/menu'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
