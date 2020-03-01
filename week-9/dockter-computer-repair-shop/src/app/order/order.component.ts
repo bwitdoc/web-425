6d7e047d474b953a2f7a10cd3fc530774047e472
Brittany Dockter <bwitdoc@gmail.com>
2020-03-01 12:44:18 -0800

    adding week 9 assignments

---
+++ week-9/dockter-computer-repair-shop/src/app/order/order.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  template: `
+  `,
+  styles: []
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
